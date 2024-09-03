acaa020c207019bddb4f2b6e73792ff99994376a
Evgeniyzuev <evgeniyzuev43@gmail.com>
2024-09-03 22:34:38 +0300

    0.12

--- src/components/ReferralSystem.tsx
+++ src/components/ReferralSystem.tsx
@@ -10,7 +10,7 @@ interface ReferralSystemProps {
 const ReferralSystem: React.FC<ReferralSystemProps> = ({ initData, userId, startParam }) => {
   const [referrals, setReferrals] = useState<string[]>([])
   const [referrer, setReferrer] = useState<string | null>(null)
-  const INVITE_URL = "https://t.me/AissistDemoBot/start"
+  const INVITE_URL = "https://t.me/AissistIncomeBot/AissistIncomeBot/start"
 
 
   useEffect(() => {
